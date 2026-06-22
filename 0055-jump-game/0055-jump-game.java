390be63900dda0c6638d9d561b56720d4fc22e79
Vidit Jain <vjviditcm115@gmail.com>
2026-06-22 18:33:53 +0530

    Time: 2 ms (88.79%), Space: 47.9 MB (61.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int m = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > m){
+                return false;
+            }
+            m = Math.max(m, i + nums[i]);
+            if(m>= nums.length-1){
+                return true;
+            }
+        }
+        return true;
+    }
+}
