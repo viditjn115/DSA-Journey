556e61964c9a7b5e08d9139dcc6295b8f73e7b25
Vidit Jain <vidit.jn.11@gmail.com>
2026-08-20 20:16:30 +0530

    Time: 0 ms (100%), Space: 42.2 MB (99.36%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open=0;
+        int close=0;
+        for(char ch : s.toCharArray()){
+            if(ch=='('){
+                open++;
+            }
+            else{
+                if(open==0){
+                    close++;
+                }
+                else{
+                    open--;
+                }
+            }
+        }
+        return open+close;
+    }
+}
